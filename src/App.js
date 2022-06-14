223945fd9912a3b76f2b56514bf067cc332336d8
yashp2 <yashp9145@gmail.com>
2022-06-15 01:00:47 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import{Navbar,Nav,Container,NavDropdown} from 'react-bootstrap';
+import{Navbar,Nav,Container,NavDropdown,Card} from 'react-bootstrap';
+import Coding from './Pages/coding/Coding';
+
+
 function App() {
   return (
     <div className="App">
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <div className="container-xxl">
+        <div className="container-xl">
         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
@@ -21,26 +24,20 @@ function App() {
               <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
             </NavDropdown>
           </Nav>
-
-{/* your page here */}
-
-
           <Nav>
             <Nav.Link href="#deets">More deets</Nav.Link>
             <Nav.Link eventKey={2} href="#memes">
-              Dank memes
+              
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
         </div>
       </Navbar>
       
-
-
-
+      <Coding/>
 
     <Navbar bg="dark" variant="dark" fixed="bottom">
-    <Container className="flex-row justify-content-center">
+    <Container className="flex-row justify-content-center foot">
       <h5 className='text-success text-center'>footer</h5>
     </Container>
   </Navbar>
