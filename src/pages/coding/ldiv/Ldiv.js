f5b61c1cce8a08908c06ac8c58b8f54360f1b294
KedarKK1 <officialkedark1@gmail.com>
2022-11-06 01:33:58 +0530

    coding ldiv done

--- src/pages/coding/ldiv/Ldiv.js
+++ src/pages/coding/ldiv/Ldiv.js
@@ -132,9 +132,10 @@ export default function Ldiv( props ) {
                             {/* Chef's coding class is very famous in Chefland.
 
                             This year X students joined his class and each student will require one chair to sit on. Chef already has Y chairs in his class. Determine the minimum number of new chairs Chef must buy so that every student is able to get one chair to sit on. */}
-                            Description - {question.description}
+                            {/* Description - {question.description} */}
+                            Description - {question.body}
                             <br />
-                            <b><h4><u>Input Constrains</u> - </h4></b>
+                            <b><h4><u>Input Format</u> - </h4></b>
                             {/* The first line contains a single integer T — the number of test cases. Then the test cases follow.
                             The first and only line of each test case contains two integers X and Y — the number of students in the class and the number of chairs Chef already has. */}
                             {/* <ul>{question.input_format.map((item) => {
@@ -148,7 +149,7 @@ export default function Ldiv( props ) {
                             )})}
                             </ul> */}
                             <br />
-                            <b><h4><u>Output Constrains</u> - </h4></b>
+                            <b><h4><u>Output Format</u> - </h4></b>
                             {question.output_format}
                             {/* For each test case, output the minimum number of extra chairs Chef must buy so that every student gets one chair. */}
                             {/* TODO */}
